47f979df7967a94ff664cea9f34751ed58984717
Tania Tatula <tgrogol@gmail.com>
2021-02-23 22:05:09 +0200

    вирішує завдання 2

--- js/task-2.js
+++ js/task-2.js
@@ -7,31 +7,19 @@
         'Приправы',
     ];
 
-    const a =[
-        'mama',
-        'tato',
-        'sister',
-    ]
-    
+   
 
     const createsListOfProducts = (products) => {
-        let number = 0;
-
-        while(number < products.length){
-            const positionEl = document.createElement('li');
-            positionEl.textContent = products[number];
-            console.log(positionEl);
-            number += 1;
-        //    ingredients.appendChild(positionEl);
+               
         const selectedById = document.querySelector('#ingredients');
-        selectedById.appendChild(positionEl);
-        }
-        // const selectedById = document.querySelector('#ingredients');
-        // selectedById.appendChild(positionEl);
-    //     const positionEl = document.createElement('li');
-    // products.forEach(product => positionEl.textContent = product);
+        const elementStringEl = products.reduce(
+            (string, item) => string + `<li>${item}</li>`,
+            ""
+        );
+        
+        selectedById.innerHTML = elementStringEl;
   }
+ 
 
-
-  createsListOfProducts(ingredients);
-  createsListOfProducts(a);
+    createsListOfProducts(ingredients);
+ 
