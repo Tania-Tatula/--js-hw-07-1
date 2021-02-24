1ada48e9d287c97afde1ce5110539c8d402fae8f
Tania Tatula <tgrogol@gmail.com>
2021-02-24 19:34:34 +0200

    завершує перший варіант вирішення задачі 3

--- js/task-2.js
+++ js/task-2.js
@@ -10,16 +10,34 @@
    
 
     const createsListOfProducts = (products) => {
-               
-        const selectedById = document.querySelector('#ingredients');
-        const elementStringEl = products.reduce(
-            (string, item) => string + `<li>${item}</li>`,
-            ""
-        );
+              
         
-        selectedById.innerHTML = elementStringEl;
+    const selectedById = document.querySelector('#ingredients');
+    
+    const liItems = products.map(product =>{
+        const positionLiEl = document.createElement('li');
+        positionLiEl.textContent = product;
+        return positionLiEl;
+    });
+    selectedById.append(...liItems)
+   
   }
+  
+// --вирішення 2--
+//   const createsListOfProducts = (products) => {
+               
+//     const selectedById = document.querySelector('#ingredients');
+//     const elementStringEl = products.reduce(
+//         (string, item) => string + `<li>${item}</li>`,
+//         ""
+//     );
+    
+//     selectedById.innerHTML = elementStringEl;
+// }
+
+
+createsListOfProducts(ingredients);
  
 
-    createsListOfProducts(ingredients);
+    
  
