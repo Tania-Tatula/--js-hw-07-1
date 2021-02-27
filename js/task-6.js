c3c4d4926a3fdf3f240b47cc88248453d4844f34
Tania Tatula <tgrogol@gmail.com>
2021-02-27 20:01:27 +0200

    частково виконує завдання 8

--- js/task-6.js
+++ js/task-6.js
@@ -1,15 +1,15 @@
-const inputEl = document.querySelector("#validation-input");
+const inputEl = document.querySelector('#validation-input');
 
 
 const handleInputFocus = (event) => {
     event.preventDefault();
 
-    if(`${event.currentTarget.value.length}` === `${inputEl.getAttribute("data-length")}`){
-        return inputEl.setAttribute("class", "valid"); 
+    if(`${event.currentTarget.value.length}` === `${inputEl.getAttribute('data-length')}`){
+        return inputEl.setAttribute('class', 'valid'); 
     }
-        return inputEl.setAttribute("class", "invalid");
+        return inputEl.setAttribute('class', 'invalid');
 
 }
 
 
-inputEl.addEventListener("blur", handleInputFocus);
+inputEl.addEventListener('blur', handleInputFocus);
--- js/task-8.js
+++ js/task-8.js
@@ -3,19 +3,7 @@ const btnDestroy = btnBox.lastElementChild;
 const btnRender = btnDestroy.previousElementSibling;
 const boxesEl = document.querySelector('#boxes');
 const numberEl = btnBox.firstElementChild;
-
-
-
-// const createBoxes =(amount =>{
-    
-//     let counter = 1;
-//     while(counter <= amount){
-//     boxesEl.insertAdjacentHTML('afterbegin', '<div class="list-styles_`${counter}`"></div>');
-//     counter += 1;
-//     }
-
-// })
-
+const boxStyles = boxesEl.children;
 
 
 const handleClearOutput = () => {
@@ -25,16 +13,37 @@ const handleClearOutput = () => {
 }
 
 const handleIntroductionNumber = () => {
+
+    // ---зробити, щоб додавало без циклу за один прихід-----
   const numberOfInputs = numberEl.value
   let counter = 1;
     while(counter <= numberOfInputs){
-    boxesEl.insertAdjacentHTML('afterbegin', '<div class="list-styles_`${counter}`"></div>');
+    boxesEl.insertAdjacentHTML('afterbegin', '<div class="list-styles"></div>');
     counter += 1;
     }
-
+    
 }
+
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+const colorNumvber = 'rgb(' +`${getRandomInt(0, 1000)}` + ',' + `${getRandomInt(0, 1000)}` + ',' + `${getRandomInt(0, 1000)}` +')' ;
+console.log(colorNumvber);
+
+
+boxStyles.style.backgroundColor = `${colorNumvber}`;
+boxStyles.style.backgroundColor = `${colorNumvber}`;
+boxStyles.style.width = '30px';
+boxStyles.style.heigt = '30px';
 
 btnDestroy.addEventListener('click', handleClearOutput);
 btnRender.addEventListener('click', handleIntroductionNumber);
 
 
+
+
+
+  
+ 
